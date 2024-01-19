c4cab57eafe056c0c0a2136029b26b356175e2bd
Austen Stone <austenstone@github.com>
2024-01-19 08:25:34 -0500

    Add Highcharts configuration and update table styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,38 @@
 import { Component } from '@angular/core';
+import ExportingModule from 'highcharts/modules/exporting';
+import SunsetTheme from 'highcharts/themes/sunset.js';
+import * as Highcharts from "highcharts";
+
+// The modules will work for all charts.
+ExportingModule(Highcharts);
+// SunsetTheme(Highcharts);
+
+Highcharts.setOptions({
+  chart: {
+    backgroundColor: 'transparent',
+  },
+  title: {
+    style: {
+      color: '#000',
+      font: '24px Roboto, sans-serif'
+    }
+  },
+  subtitle: {
+    style: {
+      color: '#666666',
+      font: '14px Roboto, sans-serif'
+    }
+  },
+  legend: {
+    itemStyle: {
+      font: '9pt Roboto, sans-serif',
+      color: 'black'
+    },
+    itemHoverStyle: {
+      color: 'gray'
+    }
+  }
+})
 
 @Component({
   selector: 'app-root',
