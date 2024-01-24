6988801886be5dbe5f07b0694ed4540da7a13b38
Austen Stone <austenstone@github.com>
2024-01-24 01:35:03 -0500

    Fix Highcharts options initialization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -216,7 +216,7 @@ export class AppComponent {
                     }
                 })
             } else {
-
+                Highcharts.setOptions(this.options);
             }
         }
         )
